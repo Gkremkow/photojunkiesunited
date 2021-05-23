d1b00afc95160c0bb87954a669cd755e8b90ab72
Gary Kremkow <gary.kremkow@gmail.com>
2021-05-23 13:32:09 -0400

    first commit

---
+++ assets/js/footer_navigation.js
@@ -0,0 +1,5 @@
+/* Javascript documentation to write and create the footer navigation bar */
+
+document.write("<div id='footerNav'>")
+document.write("<a href='../home.htm'>Back to Coures Homepage</a> - <a href='index.htm'>Home</a> - <a href='rules.html'>Rules</a> - <a href='nature.html'>Nature</a> - <a href='food.html'>Food</a> - <a href='cute_animals.html'>Cute Animals</a> - <a href='chatroom.html'>Chatroom</a> - <a href='vote.html'>Vote</a> - <a href='winners.html'>Winners Gallery</a> - <a href='contact.html'>Contact Us</a>")
+document.write("</div>")
